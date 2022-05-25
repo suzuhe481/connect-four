8d81005c9314e85f623d78ffc18853e2f326b65a
Hector <baseballhector@gmail.com>
2022-05-24 20:18:57 -0400

    Added method to print board

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,10 @@
 class Board
-  attr_accessor :width, :height, :board_arr
+  attr_accessor :width, :height, :max_height, :board_arr
 
   def initialize(width = 7, height = 0)
     @width = width
     @height = height
+    @max_height = 7
 
     @board_arr = Array.new(width) { Array.new(height) }
   end
@@ -11,5 +12,28 @@ class Board
   # Returns the value of the token at the given position.
   def get_token_at(row, column)
     @board_arr[column][row]
+  end
+
+  # Drops a token at a given column.
+  def drop_token(column, token)
+    @board_arr[column - 1].append(token)
+  end
+
+  # Prints the current game board.
+  def print_board
+    rotated_board = @board_arr
+
+    rotated_board.each do |col|
+      until col.count == @max_height
+        col.append(" ")
+      end
+    end
+
+    rotated_board = rotated_board.map(&:reverse).transpose
+
+    rotated_board.each do |row|
+      p row
+    end
+    puts ""
   end
 end
