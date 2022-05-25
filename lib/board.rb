34dd58f9a77d43df1d8d102a9fd115bba4a75c31
Hector <baseballhector@gmail.com>
2022-05-24 21:27:01 -0400

    Added method for checking matching tokens above a given position

--- lib/board.rb
+++ lib/board.rb
@@ -10,13 +10,34 @@ class Board
   end
 
   # Returns the value of the token at the given position.
-  def get_token_at(row, column)
-    @board_arr[column][row]
+  def get_token_at(row, col)
+    @board_arr[col - 1][row - 1]
   end
 
   # Drops a token at a given column.
-  def drop_token(column, token)
-    @board_arr[column - 1].append(token)
+  def drop_token(col, token)
+    @board_arr[col - 1].append(token)
+  end
+
+  # Checks for matching tokens above given token
+  def match_above(row, col)
+    curr_token = get_token_at(row, col)
+    initial_token = curr_token
+    curr_row = row
+    curr_col = col
+    puts "initial token is #{initial_token} at row: #{row} and col: #{col}"
+
+    in_a_row = 0
+    while curr_token == initial_token
+      puts "moved row"
+      in_a_row += 1
+      curr_row += 1
+
+      curr_token = get_token_at(curr_row, curr_col)
+      puts "new token is #{curr_token} at row #{curr_row} and col #{curr_col}"
+    end
+
+    in_a_row
   end
 
   # Prints the current game board.
